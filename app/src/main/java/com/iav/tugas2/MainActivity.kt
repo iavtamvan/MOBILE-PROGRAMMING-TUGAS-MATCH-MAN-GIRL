123c000b58af71070c805badfe269551d6deaee7
sandecrepo <info.sandec@gmail.com>
2019-03-06 17:38:04 +0700

    pertama upload

---
+++ app/src/main/java/com/iav/tugas2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.iav.tugas2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val putarButton: Button = findViewById(R.id.btn_putar)
+//        val diceImage: ImageView = findViewById(R.id.dice_image)
+        btn_putar.text = "Les't Go"
+        btn_putar.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
+        btn_putar.setOnClickListener {
+            kocokDadu()
+        }
+    }
+
+    private fun kocokDadu() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableRecource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableRecource)
+    }
+}
