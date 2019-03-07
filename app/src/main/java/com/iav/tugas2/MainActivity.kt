790d31962a51e9941f1180b0c2f6f7ecede7fa10
sandecrepo <info.sandec@gmail.com>
2019-03-07 23:24:27 +0700

    Adding MVP

--- app/src/main/java/com/iav/tugas2/MainActivity.kt
+++ app/src/main/java/com/iav/tugas2/MainActivity.kt
@@ -2,53 +2,16 @@ package com.iav.tugas2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.iav.tugas2.presenter.MainPresnter
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mainPresnter: MainPresnter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnMatch.setOnClickListener { pasangan() }
+
+        mainPresnter = MainPresnter()
+        btnMatch.setOnClickListener { mainPresnter.pasangan(ivCewek, ivCowok) }
     }
-
-    fun pasangan() {
-        val randomCewek = Random().nextInt(3)+1
-        val randomCowok = Random().nextInt(3)+1
-        val drawableResCewek = when(randomCewek) {
-            1 -> R.drawable.cewek1
-            2 -> R.drawable.cewek2
-            else -> R.drawable.cewek3
-        }
-        val drawableResCowok = when(randomCowok) {
-            1 -> R.drawable.cowok3
-            2 -> R.drawable.cowok1
-            else -> R.drawable.cowok2
-        }
-
-        ivDadu1.setImageResource(drawableResCewek)
-        ivDadu2.setImageResource(drawableResCowok)
-
-
-    }
-
-
-//    fun kocokDadu() {
-//        val randomInt = Random().nextInt(3) + 1
-//        val randomInt2 = Random().nextInt(3) + 1
-//        val drawableRecource = when (randomInt) {
-//            1 -> R.drawable.cewek1
-//            2 -> R.drawable.cewek2
-//            else -> R.drawable.cewek3
-//        }
-//        val drawableRecource1 = when (randomInt2) {
-//            1 -> R.drawable.cowok2
-//            2 -> R.drawable.cowok3
-//            else -> R.drawable.cowok1
-//
-//        }
-//        ivDadu1.setImageResource(drawableRecource)
-//        ivDadu2.setImageResource(drawableRecource1)
-//    }
 }
---
+++ app/src/main/java/com/iav/tugas2/presenter/MainPresnter.kt
@@ -0,0 +1,27 @@
+package com.iav.tugas2.presenter
+
+import android.widget.ImageView
+import com.iav.tugas2.R
+import java.util.*
+
+class MainPresnter {
+    fun pasangan(ivCewek:ImageView, ivCowok:ImageView ) {
+        val randomCewek = Random().nextInt(3) + 1
+        val randomCowok = Random().nextInt(3) + 1
+        val drawableResCewek = when (randomCewek) {
+            1 -> R.drawable.cewek1
+            2 -> R.drawable.cewek2
+            else -> R.drawable.cewek3
+        }
+        val drawableResCowok = when (randomCowok) {
+            1 -> R.drawable.cowok3
+            2 -> R.drawable.cowok1
+            else -> R.drawable.cowok2
+        }
+
+        ivCewek.setImageResource(drawableResCewek)
+        ivCowok.setImageResource(drawableResCowok)
+
+
+    }
+}
